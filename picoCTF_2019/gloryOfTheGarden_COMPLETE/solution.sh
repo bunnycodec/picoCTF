9c7910f2f321c4f46dfef9f3d010c88a18bbe089
bunny_Codec <sunnykumar@bunnycodec.com>
2023-09-29 01:44:27 +0530

    TheNumber in 2019 Added

--- picoCTF_2019/gloryOfTheGarden_COMPLETE/solution.sh
+++ picoCTF_2019/gloryOfTheGarden_COMPLETE/solution.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/zsh
 
 strings garden.jpg | tail -n 1 | cut -d '"' -f2
---
+++ picoCTF_2019/theNumbers_COMPLETE/get_flag.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from string import ascii_uppercase as uc
+
+numbers = [16, 9, 3, 15, 3, 20, 6, "{", 20, 8, 5, 14, 21, 13, 2, 5, 18, 19, 13, 1, 19, 15, 14, '}']
+
+flag = []
+for n in numbers:
+	if type(n) == str:
+		flag.append(n)
+	else:
+		flag.append(uc[n-1])
+
+print(''.join(flag))
+
+
